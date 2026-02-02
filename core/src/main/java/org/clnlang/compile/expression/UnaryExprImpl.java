a9c593fe4783b897a4650298aee3d930b06284d2
Gabriel Szabo <szabo.gabriel@gmail.com>
2026-02-02 22:47:27 +0100

    Fix unary expression handling - boolean

--- core/src/main/java/org/clnlang/compile/expression/UnaryExprImpl.java
+++ core/src/main/java/org/clnlang/compile/expression/UnaryExprImpl.java
@@ -25,7 +25,24 @@ public class UnaryExprImpl implements CompiledExpr {
     @Override
     public Object evaluate(ExecutionContext context) throws Exception {
         Object value = operand.evaluate(context);
-        // Perform unary operation based on operator
-        return null; // TODO: implement operator logic
+        
+        switch (operator) {
+            case "!":
+                // Boolean NOT
+                if (value instanceof Boolean) {
+                    return !(Boolean) value;
+                }
+                throw new RuntimeException("Cannot apply '!' operator to non-boolean value: " + value);
+                
+            case "-":
+                // Numeric negation
+                if (value instanceof Integer) {
+                    return -(Integer) value;
+                }
+                throw new RuntimeException("Cannot apply '-' operator to non-numeric value: " + value);
+                
+            default:
+                throw new RuntimeException("Unknown unary operator: " + operator);
+        }
     }
 }
